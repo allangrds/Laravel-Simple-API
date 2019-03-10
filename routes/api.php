2fddbf9379e534196a9dcccbf9a6943eb36ca305
Allan Ramos <allangrds@gmail.com>
2019-03-10 01:14:53 -0300

    feat(Route): add rate limit

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,19 @@
 <?php
-
-use Illuminate\Http\Request;
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::prefix('tasks')->group(function () {
-    Route::get('/', 'TaskController@index')->name('tasks.index');
-    Route::post('/', 'TaskController@store')->name('tasks.store');
-    Route::get('/{task}', 'TaskController@show')->name('tasks.show');
-    Route::patch('/{task}', 'TaskController@update')->name('tasks.update');
-    Route::delete('/{task}', 'TaskController@destroy')->name('tasks.destroy');
-});
-
-Route::prefix('auth')->group(function () {
-    Route::post('login', 'AuthController@login')->name('auth.login');
-
-    Route::middleware(['auth:api'])->group(function () {
-        Route::post('logout', 'AuthController@logout')->name('auth.logout');
+Route::middleware('throttle:15,1')->group(function () {
+    Route::prefix('tasks')->group(function () {
+        Route::get('/', 'TaskController@index')->name('tasks.index');
+        Route::post('/', 'TaskController@store')->name('tasks.store');
+        Route::get('/{task}', 'TaskController@show')->name('tasks.show');
+        Route::patch('/{task}', 'TaskController@update')->name('tasks.update');
+        Route::delete('/{task}', 'TaskController@destroy')->name('tasks.destroy');
     });
 });
 
+Route::middleware('throttle:10,1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('login', 'AuthController@login')->name('auth.login');
+        Route::middleware(['auth:api'])->group(function () {
+            Route::post('logout', 'AuthController@logout')->name('auth.logout');
+        });
+    });
+});
